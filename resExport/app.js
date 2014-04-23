2b816e3fe1bb5b77e9e32642f938aa463b609bc0
youngsheep <yysheep1101@gmail.com>
2014-04-23 20:25:07 +0800

    client res export use pb encode

--- resExport/app.js
+++ resExport/app.js
@@ -15,7 +15,6 @@ var wOption = {
 var getSheetData = function(sheets,name){
     var data = {};
     sheets.forEach(function(sheet){
-        console.log(sheet.name + " -- " + name);
         if(sheet.name == name){
             data = sheet.data;
         }
@@ -23,11 +22,13 @@ var getSheetData = function(sheets,name){
     return data;
 };
 
-var filterServerData = function (data,meta){
+var filterData = function (data,meta){
+    //console.log(data);
+    //console.log(meta);
     var cols = {};
     var keys = Object.keys(meta);
     for(var i = 0 ; i < data[0].length ; i++){
-        for(var j = 0 ; i < keys.length ; j++){
+        for(var j = 0 ; j < keys.length ; j++){
             if(data[0][i].value == keys[j]){
                 cols[keys[j]] = i;
                 break;
@@ -39,7 +40,16 @@ var filterServerData = function (data,meta){
     for(var i = 1; i < data.length; i++){
         var row = {};
         Object.keys(cols).forEach(function(ck){
-            row[ck] = data[i][cols[ck]].value;
+            if(meta[ck] == 'Number'){
+                row[ck] = Number(data[i][cols[ck]].value);
+            }
+            else if(meta[ck] == 'String'){
+                row[ck] = String(data[i][cols[ck]].value);
+            }
+            else{
+                row[ck] = data[i][cols[ck]].value;
+            }
+            
         });
         realdata.push(row);
     }
@@ -52,9 +62,9 @@ var ServerExport = function(key){
     var convparam = convlist.server[key];
     if(!!convparam){
         var xlsxFile = xlsx.parse('./xlsx/'+convparam.xlsx);
-        console.log(xlsxFile);
+        //console.log(xlsxFile);
         var data = getSheetData(xlsxFile.worksheets,convparam.sheet);
-        console.log(data);
+        //console.log(data);
         if(!data){
             console.error('sheet in file not found');
             return;
@@ -62,22 +72,59 @@ var ServerExport = function(key){
         var meta = require('./server/meta/'+convparam.meta);
         console.log(meta);
 
-        var realdata = filterServerData(data,meta);
+        var realdata = filterData(data,meta);
         console.log(realdata);
-        var buf = new Buffer(JSON.stringify(realdata));
+        var buff = new Buffer(JSON.stringify(realdata));
         var fileWriteStream = fs.createWriteStream('./server/res/'+convparam.res,wOption);
         fileWriteStream.on("error", function(err) { 
             console.error("Server Export write file error!");
             console.error(err);
         }); 
-        fileWriteStream.write(buf);
+        fileWriteStream.write(buff);
     }
 };
 
-var ClientExport = function(){
+var ClientExport = function(key){
     //var obj = xlsx.parse(__dirname + '/xlsx/test.xlsx');
     //console.log(obj.worksheets[0].data); 
     console.log('client Export!');
+
+    var convparam = convlist.client[key];
+    if(!!convparam){
+        var xlsxFile = xlsx.parse('./xlsx/'+convparam.xlsx);
+        //console.log(xlsxFile);
+        var data = getSheetData(xlsxFile.worksheets,convparam.sheet);
+        //console.log(data);
+        if(!data){
+            console.error('sheet in file not found');
+            return;
+        }
+        var meta = require('./client/meta/'+convparam.meta);
+        //console.log(meta);
+
+        var realdata = filterData(data,meta.entry);
+        console.log(realdata);
+    
+        var protobuf = require('pomelo-protobuf');
+        var resProto = protobuf.parse(meta.proto);
+        console.log(resProto);
+        protobuf.init({encoderProtos:resProto, decoderProtos:resProto});
+        var encode = {};
+        encode[convparam.proto] = realdata;
+        var buf = protobuf.encode(convparam.proto,encode);
+        console.log(buf);
+
+        var fileWriteStream = fs.createWriteStream('./client/res/'+convparam.res,wOption);
+        fileWriteStream.on("error", function(err) { 
+            console.error("Server Export write file error!");
+            console.error(err);
+        }); 
+        fileWriteStream.write(buf);
+
+        var dec = protobuf.decode("test",buf);
+        console.log("decode:");
+        console.log(dec);
+    }    
 };
 
 var list = function(val){
@@ -87,7 +134,7 @@ var list = function(val){
 program
   .version('0.0.1')
   .option('-s, --server [value]', 'export server res', list)
-  .option('-c, --client [value]', 'export client res',list)
+  .option('-c, --client [value]', 'export client res', list)
   .option('-a, --all [value]', 'export all res')
   .parse(process.argv);
 
@@ -95,7 +142,7 @@ if(program.server){
     if(program.server === true){
         program.server = Object.keys(convlist.server);
     }
-    //console.log(program.server);
+    console.log(program.server);
 
     program.server.forEach(function(key){
         ServerExport(key);
@@ -104,7 +151,14 @@ if(program.server){
 }
 
 if(program.client){
-    ClientExport();
+    if(program.client === true){
+        program.client = Object.keys(convlist.client);
+    }
+    console.log(program.client);
+
+    program.client.forEach(function(key){
+        ClientExport(key);
+    });
 }
 
 if(program.all){
@@ -112,7 +166,9 @@ if(program.all){
         ServerExport(key);
     });
     
-    ClientExport();
+    Object.keys(convlist.client).forEach(function(key){
+        ClientExport(key);
+    });
 }
 
 
